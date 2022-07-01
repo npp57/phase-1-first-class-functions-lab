c973e16033fc2f3999e8b26d97f88e7970635a87
Neal Patel <xnealx66@gmail.com>
2022-07-01 19:09:28 -0400

    commit tree be9c24752c708fc465eab2445f3356eb94ff8668

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) => {return drivers.slice(0,2)}; 
+const returnLastTwoDrivers = (drivers) => {return drivers.slice(-2)};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareMultiplier = function createFareMultiplier(multi){
+    return function(multi) {
+        return multi * multi;
+    };
+}
